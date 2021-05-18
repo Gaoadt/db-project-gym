1b57d1c244f99705340396c814c70638cd3dab7f
Gregory Minakov <goutlook.com>
2021-05-18 17:55:37 +0300

    Queries has been made

--- sql/five_queries.sql
+++ sql/five_queries.sql
@@ -1,19 +1,19 @@
 /* Выяснить какие же группы мышщ развивает программа для набора массы в алфавитном порядке */
-SELECT exercises.muscles from fitness.exercises inner join fitness.program_exercises on exercises.id = program_exercises.exercise_id
-    inner join fitness.programs on programs.id = program_exercises.program_id and programs.name = 'Для набора массы' order by muscles;
+SELECT exercise.muscles_nm from fitness.exercise inner join fitness.program_x_exercise on exercise.id = program_x_exercise.exercise_id
+    inner join fitness.program on program.id = program_x_exercise.program_id and program.program_nm = 'Для набора массы' order by muscles_nm;
 
 /* Выяснит у какого тренера занимается сколько атлетов */
-SELECT trainers.name, count(athletes.id) from fitness.trainers inner join fitness.athletes on trainers.id = athletes.trainer_id group by trainers.name;
+SELECT trainer.trainer_nm, count(athlete.id) from fitness.trainer inner join fitness.athlete on trainer.id = athlete.trainer_id group by trainer.trainer_nm;
 
 /* Выяснить сколько атлетов занимаются по определнной программе среди тех у которых нет тренера в названии которых имеет вид Для.. тралалал*/
-SELECT programs.name, count(athletes.id) from fitness.programs inner join fitness.athletes on programs.id = athletes.program_id group by programs.name
-having programs.name like 'Для%';
+SELECT program.program_nm, count(athlete.id) from fitness.program inner join fitness.athlete on program.id = athlete.program_id group by program.program_nm
+having program.program_nm like 'Для%';
 
-/* Выяснить у какого зала сколько фитнесс клубов, среди тех у кого их несколько */
-SELECT club, count(gyms.name) from fitness.gyms group by club having count(name) > 1;
+/* Выяснить у какого фитнесс клубов сколько залов, среди тех у кого их несколько */
+SELECT club_nm, count(gym.gym_title) from fitness.gym group by club_nm having count(gym.gym_title) > 1;
 
 /* Вывести список пар упражнение - программа указав для каждого упражнения число программ тренировок его включающих*/
-SELECT exercises.name, programs.name, count(*) over (partition by exercise_id) as program_count
- from fitness.exercises
-inner join fitness.program_exercises on exercises.id = program_exercises.exercise_id inner join fitness.programs
-on program_exercises.program_id = programs.id;
+SELECT exercise.exercise_nm, program.program_nm, count(*) over (partition by exercise_id) as program_count
+ from fitness.exercise
+inner join fitness.program_x_exercise on exercise.id = program_x_exercise.exercise_id inner join fitness.program
+on program_x_exercise.program_id = program.id;
